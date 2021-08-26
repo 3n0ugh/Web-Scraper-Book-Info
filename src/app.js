f2d530fcfef3931577e8b5d268218633b5606155
3n0ugh <my@emailaddress.com>
2021-08-26 13:52:02 +0000

    :sparkles: Works Well

--- src/app.js
+++ src/app.js
@@ -1,27 +1,8 @@
 const crawler = require('./crawler/crawler.js');
 
-// sleep function to bypass the request limit
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
+let pageUrls = [];
 
-// base url
 const URL =
-  'https://www.dr.com.tr/kategori/Kitap/Edebiyat/Roman/grupno=00211?ShowNotForSale=True';
+  'https://www.dr.com.tr/kategori/Kitap/Edebiyat/Roman/grupno=00211?ShowNotForSale=True&Page=106';
 
-// for (let i = 0; i < 550; i++) {
-//   const newURL = URL.concat('&Page=', i);
-//   console.log(newURL);
-//   crawler.getLinks(newURL);
-//   console.log('ok -> ' + i);
-//   sleep(5000); // wait 10 seconds after send 10 request.
-//
-// }
-
-crawler.getLinks(
-  'https://www.dr.com.tr/kategori/Kitap/Edebiyat/Roman/grupno=00211?ShowNotForSale=True&Page=30'
-);
+crawler.getLinks(URL);
--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -4,7 +4,7 @@ const { find } = require('cheerio/lib/api/traversing');
 
 const db = require('../db/connection');
 const books = db.get('book-info');
-// books.createIndex('book-name', { unique: true });
+books.createIndex('book-name', { unique: true });
 
 // Storing book links
 let links = [];
@@ -26,83 +26,6 @@ const languageTranslator = {
   ' Dil ': 'language',
   ' Barkod': 'barcode',
 };
-
-// sleep function to bypass the request limit
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-// Inserting the data into database
-function insertDatabase(bookProperties) {
-  delete bookProperties['x'];
-  delete bookProperties['_id'];
-  delete bookProperties['undefined'];
-  console.log('Inserting data...');
-  books
-    .findOne({
-      'book-name': bookProperties['book-name'],
-    })
-    .then((book) => {
-      // Error handling to prevent data duplication error.
-      if (book) {
-        const error = new Error(
-          `${bookProperties['book-name']} already exists in database.`
-        );
-        console.log(error);
-      } else {
-        books.insert(bookProperties).then(() => {
-          console.log(bookProperties);
-        });
-      }
-    });
-}
-// Getting Book Info
-function getBookInfo(url) {
-  axios
-    .get(encodeURI(url))
-    .then((response) => {
-      let $ = cheerio.load(response.data);
-
-      // Grabbing book properties
-      $('div.product-property ul.nav li').each((index, element) => {
-        const value = $(element).find('span').text();
-        const key = $(element).find($('strong')).text();
-        bookProperties[languageTranslator[key]] = value;
-      });
-
-      // Grabbing description
-      $('div.product-description-header').each((index, element) => {
-        const description = $(element).find('p:eq(1)').text();
-        bookProperties['description'] = description;
-      });
-    })
-    .then(() => {
-      console.log('Getting book information...');
-      insertDatabase(bookProperties);
-    })
-    .catch((error) => {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        // console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-    });
-}
 
 // Getting Book Links
 function getLinks(url) {
@@ -133,6 +56,83 @@ function getLinks(url) {
         console.log('Error', error.message);
       }
       console.log(error.config);
+    });
+}
+
+// Getting Book Info
+function getBookInfo(url) {
+  axios
+    .get(encodeURI(url))
+    .then((response) => {
+      let $ = cheerio.load(response.data);
+
+      // Grabbing book properties
+      $('div.product-property ul.nav li').each((index, element) => {
+        const value = $(element).find('span').text();
+        const key = $(element).find($('strong')).text();
+        bookProperties[languageTranslator[key]] = value;
+      });
+
+      // Grabbing description
+      $('div.product-description-header').each((index, element) => {
+        const description = $(element).find('p:eq(1)').text();
+        bookProperties['description'] = description;
+        insertDatabase(bookProperties);
+      });
+    })
+    // .then(() => {
+    //   console.log('Getting book information...');
+    // })
+    .then(() => {
+      console.log('done!!!');
+    })
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        // console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+// Inserting the data into database
+function insertDatabase(bookProperties) {
+  delete bookProperties['x'];
+  console.log('Inserting data...');
+  delete bookProperties['_id'];
+  delete bookProperties['undefined'];
+  books
+    .findOne({
+      'book-name': bookProperties['book-name'],
+    })
+    .then((book) => {
+      // Error handling to prevent data duplication error.
+      if (book) {
+        const error = new Error(
+          `${bookProperties['book-name']} already exists in database.`
+        );
+        console.log(error);
+      } else {
+        books
+          .insert(bookProperties)
+          .then(() => {
+            console.log('Data inserted...');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     });
 }
 module.exports = {
